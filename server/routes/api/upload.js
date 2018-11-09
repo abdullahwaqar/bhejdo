f007f4ce05889d7197e01e936c03e10c0eec66f3
awr417h <abdullahwaqar8@gmail.com>
2018-11-09 17:49:36 +0500

    Download Route added

--- server/routes/api/upload.js
+++ server/routes/api/upload.js
@@ -16,7 +16,40 @@ const storageConfig = multer.diskStorage({
 
 const upload = multer({ storage: storageConfig });
 
-router.post('/', upload.array('files'), (req, res) => {
+router.get('/', (req, res) => {
+    return res.status(200);
+});
+
+/*
+* @params(filename || token)
+* Request Method GET
+? Route for Downloading file.
+? The route accepts filename or token
+*/
+router.get('/download/:token', (req, res, next) => {
+    const filename = req.params.token;
+    const filePath = `${storageDir}/${filename}`;
+    console.log(filePath);
+    return res.download(filePath, filename, (err) => {
+        if (err) {
+            return res.status(404).json({
+                error: {
+                    message: "File Not Found."
+                }
+            });
+        } else {
+            console.log('Download Successful');
+        }
+    });
+});
+
+/*
+* @params(files.array)
+* Request Method POST
+? Route for Uploading file.
+? The route accepts array of files
+*/
+router.post('/upload', upload.array('files'), (req, res) => {
     console.log(req.files);
     const files = req.files;
     return res.json({
